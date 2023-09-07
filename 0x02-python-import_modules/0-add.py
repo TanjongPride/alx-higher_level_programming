377fb8d985f7d23846c560e1bb0a5a4742ffea48
TanjongPride <pridetanjong12@gmail.com>
2023-09-07 22:55:01 +0000

    module

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,11 @@
 #!/bin/usr/python3
-a = 1
-b = 2
+def main():
+ a = 1
+ b = 2
 
-from add_0 import add
-result = add(a, b)
+ from add_0 import add
+ result = add(a, b)
 
-print("{} + {} = {}".format(a, b, result))
+ print("{} + {} = {}".format(a, b, result))
+if __name__==__"main"__
+ main()
