8c88c18fa46e57d03efd80109f6d69e020f16a03
TanjongPride <pridetanjong12@gmail.com>
2023-09-11 21:35:18 +0000

    tuples

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def print_reversed_list_integer(my_list=[]):
+    my_list.reverse()
+    for num in my_list:
+        print("{:d}".format(num))
+my_list = [1, 2, 3, 4, 5]
+print_reversed_list_integer(my_list)
